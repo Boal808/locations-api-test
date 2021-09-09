3bf293fb9a11c35c6a3eb93bc27c5730db4c995d
Boal808 <boaler.stuart@gmail.com>
2021-09-09 11:14:06 +0300

    1st

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+// serve the public directory as a static content here
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
